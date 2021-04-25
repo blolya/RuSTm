c650994b0c280783631c58d7c88b7ca1e3e4fed1
blolya <mast-94@mail.ru>
2021-04-26 00:38:41 +0300

    Continous ranging

--- src/main.rs
+++ src/main.rs
@@ -54,73 +54,123 @@ fn main() -> ! {
     i2c1.ccr.reset_bit(15);
 
     i2c1.ccr.write_and(!0x0000_0fff);
-    i2c1.ccr.write(180);
+    i2c1.ccr.write_or(180);
 
     i2c1.trise.write(36);
 
     i2c1.cr1.set_bit(0);
-    
-    i2c1.cr1.set_bit(8);
-    while i2c1.sr1.get_bit(0) == 0 {};
-    
-    i2c1.sr1.read();
-    i2c1.sr2.read();
-    
-    i2c1.dr.write(0x29 << 1);
-    
-    while i2c1.sr1.get_bit(1) == 0 {
-        if i2c1.sr1.get_bit(10) == 1 {
-            pc13.set_low();
-        }
-    };
-    i2c1.sr1.read();
-    i2c1.sr2.read();
 
-    dbgr.send("Heyoloqwprqpwrkpqowrpqwr\r\n");
+
+    let addr = i2c_read(&i2c1, 0x0212);
+    dbgr.send("Device address is: ");
+    dbgr.send_char(addr as char);
+    dbgr.send("_____________________________________________-\r\n");
+
+
+    i2c_write(&i2c1, 0x0207, 0x01);
+    i2c_write(&i2c1, 0x0208, 0x01);
+    i2c_write(&i2c1, 0x0096, 0x00);
+    i2c_write(&i2c1, 0x0097, 0xfd);
+    i2c_write(&i2c1, 0x00e3, 0x00);
+    i2c_write(&i2c1, 0x00e4, 0x04);
+    i2c_write(&i2c1, 0x00e5, 0x02);
+    i2c_write(&i2c1, 0x00e6, 0x01);
+    i2c_write(&i2c1, 0x00e7, 0x03);
+    i2c_write(&i2c1, 0x00f5, 0x02);
+    i2c_write(&i2c1, 0x00d9, 0x05);
+    i2c_write(&i2c1, 0x00db, 0xce);
+    i2c_write(&i2c1, 0x00dc, 0x03);
+    i2c_write(&i2c1, 0x00dd, 0xf8);
+    i2c_write(&i2c1, 0x009f, 0x00);
+    i2c_write(&i2c1, 0x00a3, 0x3c);
+    i2c_write(&i2c1, 0x00b7, 0x00);
+    i2c_write(&i2c1, 0x00bb, 0x3c);
+    i2c_write(&i2c1, 0x00b2, 0x09);
+    i2c_write(&i2c1, 0x00ca, 0x09);
+    i2c_write(&i2c1, 0x0198, 0x01);
+    i2c_write(&i2c1, 0x01b0, 0x17);
+    i2c_write(&i2c1, 0x01ad, 0x00);
+    i2c_write(&i2c1, 0x00ff, 0x05);
+    i2c_write(&i2c1, 0x0100, 0x05);
+    i2c_write(&i2c1, 0x0199, 0x05);
+    i2c_write(&i2c1, 0x01a6, 0x1b);
+    i2c_write(&i2c1, 0x01ac, 0x3e);
+    i2c_write(&i2c1, 0x01a7, 0x1f);
+    i2c_write(&i2c1, 0x0030, 0x00);
+    i2c_write(&i2c1, 0x0011, 0x10);
+    i2c_write(&i2c1, 0x010a, 0x30);
+    i2c_write(&i2c1, 0x003f, 0x46);
+    i2c_write(&i2c1, 0x0031, 0xFF);
+    i2c_write(&i2c1, 0x0040, 0x63);
+    i2c_write(&i2c1, 0x002e, 0x01);
+    i2c_write(&i2c1, 0x001b, 0x09);
+    i2c_write(&i2c1, 0x003e, 0x31);
+    i2c_write(&i2c1, 0x0014, 0x24);
+
+    dbgr.send("Settings are loaded\r\n");
+
+    i2c_write(&i2c1, 0x0016, 0x00);
+
+    dbgr.send("Init complete\r\n");
 
     
+    i2c_write(&i2c1, 0x0018, 0x03);
     loop {
-        let distance = i2c_read(&i2c1, 0x0062, &pc13);
-        dbgr.send_char(distance as char);
-        dbgr.send("\r\n");
 
-        for _ in 0..10000 {};
+
+        let status = i2c_read(&i2c1, 0x4f);
+        let range_status = status & 0x07;
+
+        if range_status == 0x04 {
+
+            let data = i2c_read(&i2c1, 0x62);
+            i2c_write(&i2c1, 0x0015, 0x07);
+
+            dbgr.send_char(data as char);
+            dbgr.send("\r\n");
+        }
+
     }
 }
 
 
-fn i2c_write(i2c: &I2c1, address: u8, data: u8) {
+fn i2c_write(i2c: &I2c1, address: u32, data: u8) {
     i2c.cr1.set_bit(8);
     while i2c.sr1.get_bit(0) == 0 {};
     i2c.sr1.read();
 
-    i2c.dr.write(0x29 << 1 + 1);
+    
+    i2c.dr.write(0x29 << 1);
     while i2c.sr1.get_bit(1) == 0 {};
     i2c.sr1.read();
     i2c.sr2.read();
-		
-    i2c.dr.write(address as u32);
+    
+    i2c.dr.write(((address >> 8) & 0xff) as u32);
     while i2c.sr1.get_bit(7) == 0 {};
-
+    i2c.dr.write((address & 0xff) as u32);
+    while i2c.sr1.get_bit(7) == 0 {};
+    
     i2c.dr.write(data as u32);
     while i2c.sr1.get_bit(2) == 0 {};
     i2c.cr1.set_bit(9);	
+    
 }
 
-fn i2c_read(i2c: &I2c1, address: u8, led: &Port) -> u8 {
-
+fn i2c_read(i2c: &I2c1, address: u32) -> u8 {
+    
     i2c.cr1.set_bit(8);
     while i2c.sr1.get_bit(0) == 0 {};
     i2c.sr1.read();
 
-
-    i2c.dr.write((0x29 << 1));
+    i2c.dr.write(0x29 << 1);
     while i2c.sr1.get_bit(1) == 0 {};
     i2c.sr1.read();
     i2c.sr2.read();
 
 
-    i2c.dr.write(address as u32);
+    i2c.dr.write(((address >> 8) & 0xff) as u32);
+    while i2c.sr1.get_bit(7) == 0 {};
+    i2c.dr.write((address & 0xff) as u32);
     while i2c.sr1.get_bit(7) == 0 {};
     i2c.cr1.set_bit(9);	
 
@@ -134,8 +184,6 @@ fn i2c_read(i2c: &I2c1, address: u8, led: &Port) -> u8 {
     while i2c.sr1.get_bit(1) == 0 {};
     i2c.sr1.read();
     i2c.sr2.read();
-
-    led.set_low();
 
 		
     i2c.cr1.write_and(!0x0000_0400);
